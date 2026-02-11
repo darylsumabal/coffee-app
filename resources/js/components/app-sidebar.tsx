51bca27c7b970fa1c72767761a9850aa8cf35ac9
darylsumabal <darylsumabal123@gmail.com>
2026-02-11 15:39:10 +0800

    sidebar

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid, ShoppingBasket } from 'lucide-react';
+import { BookOpen, Folder, LayoutGrid, ShoppingBasket, User } from 'lucide-react';
 import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
@@ -23,9 +23,14 @@ const mainNavItems: NavItem[] = [
         icon: LayoutGrid,
     },
     {
-        title: 'Menu',
+        title: 'Drink',
         href: '/admin/drink',
         icon: ShoppingBasket,
+    },
+    {
+        title: 'Order',
+        href: '/admin/order',
+        icon: User,
     },
 ];
 
