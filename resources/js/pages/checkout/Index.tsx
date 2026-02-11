d8ac497b08ffbc03d1d3660d572e5d5c747a7af5
darylsumabal <darylsumabal123@gmail.com>
2026-02-11 16:57:28 +0800

    checkout:

--- resources/js/pages/checkout/Index.tsx
+++ resources/js/pages/checkout/Index.tsx
@@ -8,37 +8,43 @@ import React from 'react';
 
 const Checkout = () => {
     const { orderData } = usePage().props;
-    console.log(orderData);
+
     return (
         <AppContent>
-            <div className="flex w-full justify-evenly py-20">
-                <div className="flex w-full flex-1 flex-col">
-                    <div>
-                        <p>Checkout</p>
+            <div className="flex justify-evenly py-20">
+                <div>
+                    <div className="space-y-7">
+                        <div>
+                            <p>Checkout</p>
+                        </div>
+                        <form action="" className="flex flex-col w-96">
+                            <div>
+                                <Label>Employee ID</Label>
+                                <Input />
+                            </div>
+                            <div>
+                                <Label>Name</Label>
+                                <Input />
+                            </div>
+                            <div>
+                                <Label>Email</Label>
+                                <Input />
+                            </div>
+                        </form>
                     </div>
-                    <form action="" className="flex w-full flex-col">
-                        <div>
-                            <Label>Employee ID</Label>
-                            <Input />
-                        </div>
-                        <div>
-                            <Label>Name</Label>
-                            <Input />
-                        </div>
-                        <div>
-                            <Label>Email</Label>
-                            <Input />
-                        </div>
-                    </form>
                 </div>
 
-                <div className="flex-1">
-                    <p>Review your item</p>
+                <div>
                     <div>
-                        <img src={`${ulrSrc}/${orderData.drink.drink_image}`} />
+                        <p>Review your item</p>
+                        <div>
+                            <img
+                                src={`${ulrSrc}/${orderData.drink.drink_image}`}
+                            />
+                        </div>
+                        <div>{orderData.temperature}</div>
+                        <div>{orderData.total}</div>
                     </div>
-                    <div>{orderData.temperature}</div>
-                    <div>{orderData.total}</div>
                 </div>
             </div>
         </AppContent>
