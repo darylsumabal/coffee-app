8c1e1111db687b14354f8c29f263f729ad21c0a9
Aaron M. Castañeda <aaron.castaneda@internal.esquire.com.ph>
2026-02-11 13:08:04 +0800

    sidebar

--- resources/js/components/ui/sidebar-menu.tsx
+++ resources/js/components/ui/sidebar-menu.tsx
@@ -8,22 +8,37 @@ const coffees = [
   'Mocha',
   'Kang',
   'Bogart',
+  'Lolipop me',
+  'Barurot',
+  'Leche de macha',
+  'Capuqi',
+  'Mocha',
+  'Kang',
+  'Bogart',
+
+
 ];
 
 const SideBarMenu = () => {
   return (
-    <div className="h-screen w-64 p-4 flex flex-col">
-      <h2 className="text-2xl font-bold mb-4">Menu</h2>
-      <ul className="space-y-2">
-        {coffees.map((coffee, index) => (
-          <li
-            key={index}
-            className="p-2 rounded hover:bg-gray-300 cursor-pointer transition"
-          >
-            {coffee}
-          </li>
-        ))}
-      </ul>
+    <div className="h-screen w-64 flex flex-col">
+
+      <div className="p-6">
+        <h2 className="text-xl font-bold">Menu</h2>
+      </div>
+
+      <div className="flex-1 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-100 hover:scrollbar-thumb-gray-600 p-4">
+        <ul className="space-y-2">
+          {coffees.map((coffee, index) => (
+            <li
+              key={index}
+              className="p-2 rounded hover:bg-gray-300 cursor-pointer transition"
+            >
+              {coffee}
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
