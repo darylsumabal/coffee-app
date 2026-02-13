9aab0d6add45342e452b6a2e1773c77999fce60a
darylsumabal <darylsumabal123@gmail.com>
2026-02-13 15:02:01 +0800

    ok

--- resources/js/pages/order/table.tsx
+++ resources/js/pages/order/table.tsx
@@ -38,53 +38,74 @@ const TableOrders = ({ orders }: { orders: Orders[] }) => {
                                 <TableHead>Name</TableHead>
                                 <TableHead>Order</TableHead>
                                 <TableHead>Status</TableHead>
-                                {/* <TableHead>Position</TableHead> */}
+                                <TableHead>Position</TableHead>
                                 {auth?.user && <TableHead>Action</TableHead>}
                             </TableRow>
                         </TableHeader>
                         <TableBody>
                             {orders
                                 .slice() // create a shallow copy to avoid mutating original array
+                                // .sort((a, b) => {
+                                //     const updatedA = new Date(
+                                //         a.status.updated_at,
+                                //     ).getTime();
+                                //     const updatedB = new Date(
+                                //         b.status.updated_at,
+                                //     ).getTime();
+
+                                //     return updatedB - updatedA;
+                                // })
                                 .sort(
                                     (a, b) =>
                                         (a.status.position ?? 999) -
                                         (b.status.position ?? 999),
                                 )
-                                .map((item, index) => (
-                                    <TableRow key={index}>
-                                        <TableCell>
-                                            <div>
-                                                {item.order.customer.name}
-                                            </div>
-                                        </TableCell>
-                                        <TableCell>
-                                            <div className="flex items-center gap-3">
-                                                <Avatar size="lg">
-                                                    <AvatarImage
-                                                        src={`${ulrSrc}/${item.order.drink.drink_image}`}
-                                                        alt={
-                                                            item.order.drink
-                                                                .drink_name
-                                                        }
-                                                    />
-                                                    <AvatarFallback className="text-xs">
+                                .map((item, index) => {
+                                    const createdAt = new Date(
+                                        item.order.created_at,
+                                    );
+                                  
+                                    const now = new Date();
+                                    const diffMinutes =
+                                        (now.getTime() - createdAt.getTime()) /
+                                        (1000 * 60);
+                                    const isNew = diffMinutes < 5;
+
+                                    return (
+                                        <TableRow key={index}>
+                                            <TableCell>
+                                                <div>
+                                                    {item.order.customer.name}
+                                                </div>
+                                            </TableCell>
+                                            <TableCell>
+                                                <div className="flex items-center gap-3">
+                                                    <Avatar size="lg">
+                                                        <AvatarImage
+                                                            src={`${ulrSrc}/${item.order.drink.drink_image}`}
+                                                            alt={
+                                                                item.order.drink
+                                                                    .drink_name
+                                                            }
+                                                        />
+                                                        <AvatarFallback className="text-xs">
+                                                            {
+                                                                item.order.drink
+                                                                    .drink_name
+                                                            }
+                                                        </AvatarFallback>
+                                                    </Avatar>
+                                                    <div className="font-medium">
                                                         {
                                                             item.order.drink
                                                                 .drink_name
                                                         }
-                                                    </AvatarFallback>
-                                                </Avatar>
-                                                <div className="font-medium">
-                                                    {
-                                                        item.order.drink
-                                                            .drink_name
-                                                    }
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </TableCell>
+                                            </TableCell>
 
-                                        <TableCell>
-                                            {/* <div>
+                                            <TableCell>
+                                                {/* <div>
                                                 <Badge
                                                     variant={`${item.status.status ? 'default' : 'destructive'}`}
                                                 >
@@ -93,40 +114,46 @@ const TableOrders = ({ orders }: { orders: Orders[] }) => {
                                                         : 'Pending'}
                                                 </Badge>
                                             </div> */}
-                                            <div className="relative inline-block">
-                                                {/* Badge */}
-                                                <Badge
-                                                    variant={
-                                                        item.status.status
-                                                            ? 'default'
-                                                            : 'destructive'
-                                                    }
-                                                    className="relative px-4 py-1"
-                                                >
-                                                    {item.status.status
-                                                        ? 'Ready'
-                                                        : 'Pending'}
-                                                </Badge>
 
-                                                {/* Number at top-right */}
-                                                <div className="absolute -top-2 -right-2 flex h-5 w-7 items-center justify-center rounded-md bg-emerald-600 text-xs  text-white">
-                                                    new
+                                                <div className="relative inline-block">
+                                                    {/* Badge */}
+                                                    <Badge
+                                                        variant={
+                                                            item.status.status
+                                                                ? 'default'
+                                                                : 'destructive'
+                                                        }
+                                                        className="relative px-4 py-1"
+                                                    >
+                                                        {item.status.status
+                                                            ? 'Ready'
+                                                            : 'Pending'}
+                                                    </Badge>
+
+                                                    {/* Number at top-right */}
+                                                    {isNew && (
+                                                        <div className="absolute -top-2 -right-2 flex h-5 w-7 items-center justify-center rounded-md bg-emerald-600 text-xs text-white">
+                                                            new
+                                                        </div>
+                                                    )}
                                                 </div>
-                                            </div>
-                                        </TableCell>
-                                        {/* <TableCell>
-                                            {item.status?.position}
-                                        </TableCell> */}
-                                        {auth?.user && (
-                                            <TableCell>
-                                                <SwitchToggleLabel
-                                                    status={item.status.status}
-                                                    id={item.status.id}
-                                                />
                                             </TableCell>
-                                        )}
-                                    </TableRow>
-                                ))}
+                                            <TableCell>
+                                                {item.status?.position}
+                                            </TableCell>
+                                            {auth?.user && (
+                                                <TableCell>
+                                                    <SwitchToggleLabel
+                                                        status={
+                                                            item.status.status
+                                                        }
+                                                        id={item.status.id}
+                                                    />
+                                                </TableCell>
+                                            )}
+                                        </TableRow>
+                                    );
+                                })}
                         </TableBody>
                     </Table>
                 </div>
